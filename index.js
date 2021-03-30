6563ff16e935840733d782d6ee0af0af6f39de52
Rosa Truneh <rosa.truneh@gmail.com>
2021-03-29 21:26:35 -0500

    adjusted javascript

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-/* This script will customize the greeting depending on time of day*/
+function sayHello() { 
+    document.write ('Hello');
+}
+
+
+
+// This script will customize the greeting depending on time of day*/
 
 var today = new Date();
 var hourNow = today.getHours();
